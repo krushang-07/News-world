491afe5943d96e56aecbbc7ecde1768cd52d41bc
krushang-07 <krushangsavaliya45@gmail.com>
2023-12-16 15:00:26 +0530

    use routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,115 @@
-import React, { Component } from 'react'
-import NavBar from './components/NavBar'
-import News from './components/News'
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <News/>
-      </div>
-    )
+      <BrowserRouter>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route
+              exect
+              path="/"
+              element={
+                <News key="home" pagesize={6} country="in" category="" />
+              }
+            />
+            <Route
+              exect
+              path="/general"
+              element={
+                <News
+                  key="general"
+                  pagesize={6}
+                  country="in"
+                  category="general"
+                />
+              }
+            />
+            <Route
+              exect
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pagesize={6}
+                  country="in"
+                  category="business"
+                />
+              }
+            />
+            <Route
+              exect
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pagesize={6}
+                  country="in"
+                  category="science"
+                />
+              }
+            />
+            <Route
+              exect
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pagesize={6}
+                  country="in"
+                  category="health"
+                />
+              }
+            />
+            <Route
+              exect
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pagesize={6}
+                  country="in"
+                  category="sports"
+                />
+              }
+            />
+            <Route
+              exect
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pagesize={6}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            />
+            {/* 
+            <Route
+              exect
+              path="/About"
+              key="About"
+              element={
+                <News pagesize={6} country="in" category="About" />
+              }
+            /> */}
+
+            <Route
+              exect
+              path="/technology"
+              key="technology"
+              element={<News pagesize={6} country="in" category="technology" />}
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultProps = {
+    //pagesize: 6,
+    country: "in",
+    category: "general",
+  };
 
- //use constructor for the class rander
+  PropTypes = {
+    pagesize: PropTypes.number,
+    country: PropTypes.string,
+    category: PropTypes.string,
+  };
+
+  //use constructor for the class rander
 
   constructor() {
     super();
@@ -17,8 +29,7 @@ export class News extends Component {
   //use life cycle method for the fatch the api
 
   async componentDidMount() {
-    let Url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=77719c4419594f18b76ec8e3dd22ae61";
+    let Url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=77719c4419594f18b76ec8e3dd22ae61&pagesize=${this.props.pagesize}`;
     let data = await fetch(Url);
     let parseddata = await data.json();
     console.log(parseddata);
@@ -33,9 +44,13 @@ export class News extends Component {
 
   PreviousButton = async () => {
     console.log("previous");
-    let Url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=77719c4419594f18b76ec8e3dd22ae61&page=${
+    let Url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=77719c4419594f18b76ec8e3dd22ae61&page=${
       this.state.page - 1
-    }&pagesize=20`;
+    }&pagesize=${this.props.pagesize}`;
     let data = await fetch(Url);
     let parseddata = await data.json();
 
@@ -50,12 +65,16 @@ export class News extends Component {
   NextButton = async () => {
     console.log("next");
 
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pagesize)) {
       //use math.ceil for the round of the total page size  cuz render the page equal number of page size
     } else {
-      let Url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=77719c4419594f18b76ec8e3dd22ae61&page=${
+      let Url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=77719c4419594f18b76ec8e3dd22ae61&page=${
         this.state.page + 1
-      }&pagesize=20`;
+      }&pagesize=${this.props.pagesize}`;
       let data = await fetch(Url);
       let parseddata = await data.json();
       console.log(parseddata);
@@ -88,14 +107,15 @@ export class News extends Component {
             <div className="container d-flex justify-content-between">
               <button
                 type="button "
-                class="btn btn-dark "
+                className="btn btn-dark "
                 onClick={this.PreviousButton}
+                disabled={this.state.page < 1}
               >
                 Previous
               </button>
               <button
                 type="button"
-                class="btn btn-dark"
+                className="btn btn-dark"
                 onClick={this.NextButton}
               >
                 next
